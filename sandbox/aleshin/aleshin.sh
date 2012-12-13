f43deb1116016bf6bdd5197d8ab2c4ba6162eacf
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2012-12-13 02:03:51 +0100

    Removed sandbox

--- sandbox/aleshin/aleshin.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-(echo $*; cat) | ssh lbartho@obssf3.unige.ch env LD_LIBRARY_PATH=/unige/nag/SunOS_5.8_sun4u/lib:/unige/workshop_6.0.2/SUNWspro/lib aleshin/aleshin
--- sandbox/spectrum/code.g
+++
@@ -1,4 +0,0 @@
-f := function(x)
-    if Length(x[1])=2 then return [x[10][1],x[10][2]]; fi;
-    Error("not ok");
-end;
