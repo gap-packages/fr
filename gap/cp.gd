5210ccd1d154ee5636596ae3d674af9ab64837f7
Thorsten Groth <thorsten__groth@hotmail.com>
2014-10-06 16:44:50 +0200

    Method selection for Grigorchuk group changed.
    Using now new attribute 'FRConjugacyAlgorith' instead of name.

--- gap/cp.gd
+++ gap/cp.gd
@@ -1,5 +1,7 @@
 DeclareOperation("IteratedOrbit", [ IsFRElement, IsObject]);
 DeclareAttribute("OrbitSignalizer", IsFRElement);
+################### Groups with special algorithm   ###########
+DeclareAttribute("FRConjugacyAlgorithm", IsFRGroup);
 ################### Branch Groups #############################
 DeclareGlobalVariable("START_CP_BRANCH@");
 DeclareGlobalFunction("GrigorchukConjugateBranchInit");
