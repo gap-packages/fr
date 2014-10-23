daa79eedd21d68b1b1eca412284fc5be47f4ead7
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2014-10-23 21:34:15 +0200

    Added headers to Thorsten's files

--- gap/cp.gd
+++ gap/cp.gd
@@ -1,7 +1,36 @@
+#############################################################################
+##
+#W cp.gd                                                       Thorsten Groth
+##
+#Y Copyright (C) 2014, Thorsten Groth
+##
+#############################################################################
+##
+##  This file declares some attributes for the conjugacy problem
+##
+#############################################################################
+
+## <#GAPDoc Label="CP">
+## <ManSection>
+##   <Attr Name="OrbitSignalizer" Arg="g"/>
+##   <Returns>...</Returns>
+##   <Description>
+##     ...
+## <Example><![CDATA[
+## gap> OrbitSignalizer(...);
+## ...
+## ]]></Example>
+##   </Description>
+## </ManSection>
 DeclareAttribute("OrbitSignalizer", IsFRElement);
-################### Groups with special algorithm   ###########
+##
+## <ManSection>
+### ...
+## </ManSection>
 DeclareAttribute("FRConjugacyAlgorithm", IsFRGroup,2);
-################### Branch Groups #############################
+##
+## <ManSection>
+### ...
+## </ManSection>
 DeclareAttribute("FRBranchGroupConjugacyData", IsFRGroup,"mutable");
-
-
+## <#/GAPDoc>
